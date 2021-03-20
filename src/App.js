b1e68a504b52810d7b3e8f7a36c69da1930a17e7
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-03-20 11:23:57 +0600

    Google login added with context

--- src/App.js
+++ src/App.js
@@ -9,27 +9,39 @@ import Login from './Components/Login/Login';
 import Destination from './Components/Destination/Destination';
 import NoMatch from './Components/NoMatch/NoMatch';
 import Header from './Components/Header/Header';
+import firebase from "firebase/app";
+import firebaseConfig from './Components/Config/firebaseConfig'
+import { createContext, useState } from 'react';
+
+
+export const UserContext = createContext()
 
 function App() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  const [loggedInUser, setLoggedInUser] = useState({})
   return (
-
-    <Router>
-      <Header></Header>
-      <Switch>
-        <Route exact path='/'>
-          <Home></Home>
-        </Route>
-        <Route path='/login'>
-          <Login></Login>
-        </Route>
-        <Route path="/destination">
-          <Destination></Destination>
-        </Route>
-        <Route path='*'>
-          <NoMatch></NoMatch>
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <p> Hello {loggedInUser.displayName}</p>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path='/login'>
+            <Login></Login>
+          </Route>
+          <Route path="/destination">
+            <Destination></Destination>
+          </Route>
+          <Route path='*'>
+            <NoMatch></NoMatch>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import { UserContext } from '../../App';
+
 
 const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+    const handleGoogleLogin = () => {
+        console.log('loging with google')
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                const user = result.user;
+                console.log(user)
+                const { displayName, email } = user
+                const signedInUser = { displayName, email }
+                console.log(signedInUser)
+                setLoggedInUser(signedInUser)
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.email;
+                const credential = error.credential;
+                console.log(errorCode, errorMessage, email, credential)
+            });
+    }
     return (
         <div>
-            <h2>This is Login Page</h2>
+            <button onClick={handleGoogleLogin}>Login with Google</button>
         </div>
     );
 };
